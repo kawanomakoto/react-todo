6ae70b2f10ec7404ad4ede1e445e96646d1a2e19
kawanomakoto <makoto.kawano@boaderdline.jp>
2021-08-08 21:48:26 +0900

    input追加

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -35,6 +35,7 @@ const TaskInput: React.FC<Props> = ({ setTasks, tasks }) => {
         <div>
             <div className="inputForm">
                 <div className="inner">
+                    <input type="text" className="input" />
                     <input
                         type="text"
                         className="input"
